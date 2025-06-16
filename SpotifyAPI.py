176f05db4a688e9ab193b665e1ac6506cedd1e7c
Haydos1969 <hayden.abrahams360@gmail.com>
2025-06-17 02:14:36 +0930

    moved class SpotifyAPI to an external file

---
+++ SpotifyAPI.py
@@ -0,0 +1,36 @@
+import requests
+
+class SpotifyAPI:
+  def __init__(self):
+    # Reads the .env and initalises the api key and client ID
+    env_vars = {}
+    with open(".env") as f:
+        for line in f:
+            line = line.strip()
+            if line and not line.startswith("#"):
+                key, value = line.split("=", 1)
+                env_vars[key.strip()] = value.strip()
+    self.API_KEY = env_vars["API_KEY"]
+    self.CLIENT_ID = env_vars["CLIENT_ID"]
+
+  
+  def requestAccessToken(self):
+    url = "https://accounts.spotify.com/api/token"
+    data = {
+       "grant_type" : "client_credentials",
+       "client_id" : self.CLIENT_ID,
+       "client_secret" : self.API_KEY
+    }
+    
+    response = requests.post(url, data=data)
+
+    if response.ok:
+       accessToken = response.json()
+       self.accessToken = accessToken.get("access_token")
+       return [response.status_code, response.text]
+    else:
+       return [response.status_code, response.text]
+    
+  def getRequest(self, url, headers):
+      response = requests.get(url=url, headers=headers)
+      return response.text, response.status_code
