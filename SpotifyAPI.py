1ff70c28bd2f19bf68f4c4cf44811c183e5ae069
Haydos1969 <hayden.abrahams360@gmail.com>
2025-06-17 16:15:16 +0930

    Added user auth

--- SpotifyAPI.py
+++ SpotifyAPI.py
@@ -1,20 +1,22 @@
 import requests
+import spotipy
+from spotipy.oauth2 import SpotifyOAuth
 
 class SpotifyAPI:
-  def __init__(self):
-    # Reads the .env and initalises the api key and client ID
-    env_vars = {}
-    with open(".env") as f:
-        for line in f:
-            line = line.strip()
-            if line and not line.startswith("#"):
-                key, value = line.split("=", 1)
-                env_vars[key.strip()] = value.strip()
-    self.API_KEY = env_vars["API_KEY"]
-    self.CLIENT_ID = env_vars["CLIENT_ID"]
+   def __init__(self):
+      # Reads the .env and initalises the api key and client ID
+      env_vars = {}
+      with open(".env") as f:
+         for line in f:
+               line = line.strip()
+               if line and not line.startswith("#"):
+                  key, value = line.split("=", 1)
+                  env_vars[key.strip()] = value.strip()
+      self.API_KEY = env_vars["API_KEY"]
+      self.CLIENT_ID = env_vars["CLIENT_ID"]
 
   
-  def requestAccessToken(self):
+   def requestAccessToken(self):
     url = "https://accounts.spotify.com/api/token"
     data = {
        "grant_type" : "client_credentials",
@@ -30,7 +32,16 @@ class SpotifyAPI:
        return [response.text, response.status_code]
     else:
        return [response.text, response.status_code]
-    
-  def getRequest(self, url, headers):
-      response = requests.get(url=url, headers=headers)
-      return response.json(), response.status_code
+   
+   def authenticateUser(self):
+      sp = spotipy.Spotify(auth_manager=SpotifyOAuth(
+         client_id=self.CLIENT_ID,
+         client_secret= self.API_KEY,
+         redirect_uri="http://127.0.0.1:8888/callback",
+         scope="user-library-read user-read-private"
+      ))
+      self.authUser = sp
+
+   def getRequest(self, url, headers):
+         response = requests.get(url=url, headers=headers)
+         return response.json(), response.status_code
--- main.py
+++ main.py
@@ -2,7 +2,10 @@ import requests
 from SpotifyAPI import SpotifyAPI
   
 API = SpotifyAPI()
-print(API.requestAccessToken())
+#print(API.requestAccessToken())
+API.authenticateUser()
+
+print(API.authUser.current_user())
 
 headers = {
     "Authorization": f"Bearer {API.accessToken}"
@@ -10,9 +13,3 @@ headers = {
 
 spotifyID = "nh6wd77lqlracq6pb203uol6o"#input("Enter Your Spotify ID: ")
 url = "https://api.spotify.com/v1/users/" + spotifyID + "/playlists"
-
-
-text, code = API.getRequest(url, headers)
-print(type(text))
-for i in text["items"]:
-  print(i["name"])
